ac36b693a25d752e2256c2ae62188399bd2867f9
Patrickhell <patrickhell19@yahoo.fr>
2023-05-16 01:51:29 +0200

    inserzione del form per i 2 imput

--- js/trainTiket.js
+++ js/trainTiket.js
@@ -4,36 +4,42 @@ const button = document.querySelector (".button");
 const costByKm = 0.233; 
 const travelCostBase= inputDistance.value * costByKm ;
 let result;
+let message;
 
 
 
 
 if( inputAge.value < 18) {
-    result = travelCostBase - (0.194 * travelCostBase );
+    result = travelCostBase - (0.194 * travelCostBase ); message = " : Le è stato applicato uno sconto del 20%";
+
 } else if( inputAge.value > 65) {
-   result = travelCostBase - (0.377 * travelCostBase);
+   result = travelCostBase - (0.377 * travelCostBase); message = " : Le è stato applicato uno sconto del 40%";
+
 } else {
-    result = travelCostBase;
+    result = travelCostBase ; message = "" ;
 }
 
-console.log(result. toFixed(2));
+console.log(result. toFixed(2)) + " Euro" ;
 
 
 button.addEventListener ('click' , function()  {
-    const outputElement = document.querySelector("pre");
-    outputElement.innerHTML += travelCostBase  ;
+    const outputElement = document.querySelector ("pre");
+    outputElement.innerHTML = travelCostBase  ;
     
     
 
     if( inputAge.value < 18) {
-        outputElement.innerHTML = (travelCostBase - (0.194 * travelCostBase )).toFixed(2);
+        outputElement.innerHTML = (travelCostBase - (0.194 * travelCostBase )).toFixed(2) + " Euro."  + " Le è stato applicato lo sconto del 20%";
         outputElement.classList.add ("green");
+
     } else if( inputAge.value > 65) {
-       outputElement.innerHTML = (travelCostBase - (0.377 * travelCostBase)) .toFixed(2) ;
-       outputElement.classList.add ("cyan");
-    }  else {outputElement.innerHTML = travelCostBase .toFixed(2);
+       outputElement.innerHTML = (travelCostBase - (0.377 * travelCostBase)) .toFixed(2) + " Euro."  + " Le è stato applicato lo sconto del 40%";
+       outputElement.classList.add ("cyan") ; 
+
+    }  else {outputElement.innerHTML = travelCostBase .toFixed(2) + " Euro";
         outputElement.classList.add ("orange");
     }
+
     }
 
     );
